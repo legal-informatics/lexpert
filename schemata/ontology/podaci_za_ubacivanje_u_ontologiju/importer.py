9acf222450b4b1eb70dda5745c13e92a1bcb155c
Marko Vukovic <44498569+vukovic-marko@users.noreply.github.com>
2020-04-09 20:05:59 +0200

    Izmenjena šema ontologije
    Klase su preimenovane tako da su sada sve u jednini i na engleskom jeziku (klasa Skupstine je postala Assembly, Glasila - Gazette i sl), dodata su svojstva label za jezike i države. Privremeni URL-ovi su izmenjeni tako da budu uniformni. Deo šeme koji se odnosi na događaje i uloge je izmenjen, tako da su instance klasa FRBRWork, FRBRExpression i FRBRManifestation pomoću svojstva has_related_event povezani sa instancama klase TLCEvent, dok su instance klase TLCEvent povezane svojstvom has_responsible_role sa instancama klase TLCRole (i njenim potklasama Author, Editor i Generator), gde se uloga odgovornom licu uspostavlja kreiranjem instance odgovarajuće potklase klase TLCRole i dodeljivanjem instance odgovornog lica svojstvu is_of_role te klase (TLCRole).

--- schemata/ontology/podaci_za_ubacivanje_u_ontologiju/importer.py
+++ schemata/ontology/podaci_za_ubacivanje_u_ontologiju/importer.py
@@ -7,8 +7,8 @@ counter = 1
 with open("glasila.txt", "r", encoding="utf-8") as file:
     glasila = file.readlines()
     for g in glasila:
-        iri = "gl_" + str(counter).zfill(3)
-        idividual = onto.Glasila(iri)
+        iri = "gazette_" + str(counter).zfill(3)
+        idividual = onto.Gazette(iri)
         idividual.has_name = [g.strip()]
         counter = counter + 1
 
@@ -18,7 +18,7 @@ counter = 1
 with open("vrste_formata.txt", "r", encoding="utf-8") as file:
     vrste_formata = file.readlines()
     for g in vrste_formata:
-        iri = "vf_" + str(counter).zfill(3)
+        iri = "format_" + str(counter).zfill(3)
         idividual = akn_meta.FRBRformat(iri)
         idividual.has_name = [g.strip()]
         counter = counter + 1
@@ -29,7 +29,7 @@ counter = 1
 with open("vrste_dokumenata.txt", "r", encoding="utf-8") as file:
     vrste_dokumneata = file.readlines()
     for g in vrste_dokumneata:
-        iri = "vd_" + str(counter).zfill(3)
+        iri = "subtype_" + str(counter).zfill(3)
         idividual = akn_meta.FRBRsubtype(iri)
         idividual.has_name = [g.strip()]
         counter = counter + 1
@@ -40,15 +40,16 @@ with open("zemlje.txt", "r", encoding="utf-8") as file:
     zemlje = file.readlines()
     for g in zemlje:
         g = g.split('\t')
-        akn_meta.FRBRcountry(g[0])
-
+        individual = akn_meta.FRBRcountry(g[0])
+        individual.label = [g[1].strip()]
 
 # ubacivanje liste jezika u ontologiju
 with open("jezici.txt", "r", encoding="utf-8") as file:
     jezici = file.readlines()
     for g in jezici:
         g = g.split('\t')
-        akn_meta.FRBRlanguage(g[0])
+        individual = akn_meta.FRBRlanguage(g[0])
+        individual.label = [g[2].strip()]
 
 #ubacivanje skupstina u ontologiju
 counter = 1
@@ -59,22 +60,22 @@ with open("skupstine.txt", "r", encoding="utf-8") as file:
     for s in lines:
         tabs_temp = len(s) - len(s.lstrip(' '))
         if first is None:
-            iri = "sk_" + str(counter).zfill(3)
-            first = individual = onto.Skupstine(iri)
+            iri = "assembly_" + str(counter).zfill(3)
+            first = individual = onto.Assembly(iri)
             individual.has_name = [s.strip()]
             counter = counter + 1
 
         else:
             if tabs_temp == 4:
-                iri = "sk_" + str(counter).zfill(3)
-                second = individual = onto.Skupstine(iri)
+                iri = "assembly_" + str(counter).zfill(3)
+                second = individual = onto.Assembly(iri)
                 individual.has_name = [s.strip()]
                 individual.has_superior = [first]
                 counter = counter + 1
 
             elif tabs_temp == 8:
-                iri = "sk_" + str(counter).zfill(3)
-                individual = onto.Skupstine(iri)
+                iri = "assembly_" + str(counter).zfill(3)
+                individual = onto.Assembly(iri)
                 individual.has_name = [s.strip()]
                 individual.has_superior = [second]
                 counter = counter + 1
@@ -91,17 +92,17 @@ with open("podregistar_oblast_grupa.txt", "r", encoding="utf-8") as file:
     counter_g = 1
     for g in lines:
         if '\t\t' in g:
-            grupa = onto.Grupa("grupa_" + str(counter_g).zfill(3))
+            grupa = onto.Group("group_" + str(counter_g).zfill(3))
             counter_g = counter_g + 1
             grupa.broader = [oblast]
             grupa.has_name = [' '.join(g.strip().split(' ')[1:])]
         elif '\t' in g:
-            oblast = onto.Oblast("oblast_" + str(counter_o).zfill(3))
+            oblast = onto.Field("field_" + str(counter_o).zfill(3))
             counter_o = counter_o + 1
             oblast.broader = [podregistar]
             oblast.has_name = [' '.join(g.strip().split('\t')[1:])]
         else:
-            podregistar = onto.Podregistar("podregistar_" + str(counter_p).zfill(3))
+            podregistar = onto.Subregister("subregister_" + str(counter_p).zfill(3))
             counter_p = counter_p + 1
             podregistar.has_name = [g.strip()]
 
@@ -109,16 +110,32 @@ with open("event_type.txt", "r", encoding="utf-8") as file:
     lines = file.readlines()
     counter = 1
     for e in lines:
-        eventType = onto.EventType("et_" + str(counter).zfill(3))
+        eventType = onto.EventType("eventtype_" + str(counter).zfill(3))
         counter = counter + 1
         eventType.has_name = [e.strip()]
-        
-with open("role_type.txt", "r", encoding="utf-8") as file:
+
+with open("osobe.txt", "r", encoding="utf-8") as file:
     lines = file.readlines()
     counter = 1
-    for r in lines:
-        roleType = onto.RoleType("rt_" + str(counter).zfill(3))
+    for o in lines:
+        person = akn_meta.TLCPerson("person_" + str(counter).zfill(3))
         counter = counter + 1
-        roleType.has_name = [r.strip()]
+        person.has_name = [o.strip()]
+
+with open("organizacije.txt", "r", encoding="utf-8") as file:
+    lines = file.readlines()
+    counter = 1
+    for o in lines:
+        organization = akn_meta.TLCOrganization("organization_" + str(counter).zfill(3))
+        counter = counter + 1
+        organization.has_name = [o.strip()]
+        
+# with open("role_type.txt", "r", encoding="utf-8") as file:
+#     lines = file.readlines()
+#     counter = 1
+#     for r in lines:
+#         roleType = onto.RoleType("rt_" + str(counter).zfill(3))
+#         counter = counter + 1
+#         roleType.has_name = [r.strip()]
 
 onto.save(file = "akn_meta_combined_full.owl", format = "rdfxml")
