592753640cbd8787ba08f53153e4440738a070da
Filip Jakovljevic <fillix96@gmail.com>
2020-04-15 15:37:03 +0200

    dodato brisanje za work

--- browser/flask_backend/server_1/flaskr/run_server.py
+++ browser/flask_backend/server_1/flaskr/run_server.py
@@ -1,6 +1,7 @@
 import os 
 from flask import Flask, url_for, request, jsonify, abort
 from SPARQLWrapper import SPARQLWrapper, XML, JSON
+import xmltodict
 
 app = Flask(__name__)
 
@@ -25,11 +26,24 @@ default_lng = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_core#
 def transform(data, ret_format):
 
     if ret_format == 'xml':
-        return data, 200, {'content-type':'application/xml'}
+        return data, 200, {'content-type': 'application/xml'}
 
     # TODO transformacija u ostale formate
+    if ret_format == 'xhtml':
+        return data, 200, {'content-type': 'application/xhtml+xml'}
+
+    if ret_format == 'pdf':
+        return data, 200, {'content-type': 'application/pdf'}
 
     return data
+
+
+def parse_xml(data):
+    content_dict = xmltodict.parse(data)
+
+    # TODO parsiranje xml-a i vracanje rdf grafa za ubacivanje u Fuseki
+
+    return content_dict
 
 
 @app.route('/simple_search')
@@ -39,18 +53,18 @@ def simple_search():
 
     return 'Najlepsa!'
 
-@app.route('/search')
+@app.route('/search', methods=['POST'])
 def search():
 
     # TODO search preko forme
 
-    return 'Najlepsa!'
+    return 'Svetlucava'
 
 
 # exposing Apche Jena Fuseki endpoint for SPARKQL queries
 @app.route('/sparql', methods=['POST'])
 def sparql():
-    query = request.json['query']
+    query = request.data
 
     if not query:
         abort(400, 'No sparql query has been pased')
@@ -59,19 +73,19 @@ def sparql():
 
     try :
         response = sparql_query.query().convert()
-        return response.toxml()
+        return response
     except Exception as e:
         abort(400, e)
 
 
 # endpoint for Manifestations   -->   #/akn/rs/act/zakon/2017-09-27/2017-88-3634/srp@2017-09-29/2020-03-17.xml
-@app.route('/akn/rs/act/<subtype>/<date_1>/<number>/<lng>@<date_2>/<date_3>.<ret_format>', methods=['GET', 'DELETE', 'PUT'])
+@app.route('/akn/rs/act/<subtype>/<date_1>/<number>/<lng>@<date_2>/<date_3>.<ret_format>', methods=['GET', 'DELETE', 'PUT', 'POST'])
 def fetch_manifestation(subtype, date_1, number, lng, date_2, date_3, ret_format):
-    uri = '#/akn/rs/act/' + subtype + '/' + date_1 + '/' + number + '/' + lng + '@' + date_2 + '/' + date_3 + '.xml'
+    uri = ontology_url + '#/akn/rs/act/' + subtype + '/' + date_1 + '/' + number + '/' + lng + '@' + date_2 + '/' + date_3 + '.xml'
     
     # get only returns data
     if request.method == "GET":
-        query = 'JSON {"data": ?data} WHERE { <' + ontology_url + uri + '> <' + content + '> ?data.}'
+        query = 'JSON {"data": ?data} WHERE { <'+ uri +'> <'+ content +'> ?data.}'
         sparql_query.setQuery(query)
 
         try :
@@ -82,29 +96,30 @@ def fetch_manifestation(subtype, date_1, number, lng, date_2, date_3, ret_format
 
     # deleting of all connected stuff with individual
     if request.method == "PUT" or request.method == "DELETE":
-        query = 'DELETE { ?s ?q ?o } WHERE { ?s ?q ?o. FILTER ( ?s = <'+ ontology_url + uri +'> || ?o = <'+ ontology_url + uri +'>)}'
+        query = 'DELETE { ?s ?q ?o } WHERE { ?s ?q ?o. FILTER ( ?s = <'+ uri +'> || ?o = <'+ uri +'>)}'
         sparql_update.setQuery(query)
 
         try :
             sparql_update.query()
             if request.method == "DELETE":
-                return 'Success', 204
+                return 'Manifestation deleted', 200
         except Exception as e:
             abort(400, e)
 
     # TODO insertovanje posle PUT
+    rdf_qraph = parse_xml(request.data)
 
-    return 'delete'
+    return rdf_qraph
 
 
 # endpoint for Expressions   -->   #/akn/rs/act/zakon/2017-09-27/2017-88-3634/srp@2017-09-29
-@app.route('/akn/rs/act/<subtype>/<date_1>/<number>/<lng>@<date_2>', methods=['GET', 'DELETE', 'PUT'])
+@app.route('/akn/rs/act/<subtype>/<date_1>/<number>/<lng>@<date_2>', methods=['GET', 'DELETE', 'PUT', 'POST'])
 def fetch_expression(subtype, date_1, number, lng, date_2):
-    uri = '#/akn/rs/act/' + subtype + '/' + date_1 + '/' + number + '/' + lng + '@' + date_2
+    uri = ontology_url + '#/akn/rs/act/' + subtype + '/' + date_1 + '/' + number + '/' + lng + '@' + date_2
     
     # get only returns data
     if request.method == "GET":
-        query = 'JSON {"data": ?data} WHERE { <'+ ontology_url + uri +'> <'+ embodied_in +'> ?manifestation. ' \
+        query = 'JSON {"data": ?data} WHERE { <'+ uri +'> <'+ embodied_in +'> ?manifestation. ' \
                                             '?manifestation <' + has_date + '> ?date. ' \
                                             '?manifestation <' + content + '> ?data. } ORDER BY DESC (?date)'
         sparql_query.setQuery(query)
@@ -115,35 +130,37 @@ def fetch_expression(subtype, date_1, number, lng, date_2):
         except Exception as e:
             abort(400, e)
 
-    # deleting of all connected stuff wit individual
+    # deleting of all connected stuff with individual
     if request.method == "PUT" or request.method == "DELETE":
-        query = 'DELETE { ?s ?q ?o } WHERE { {?s ?q ?o. FILTER ( ?s = <'+ ontology_url + uri +'> || ?o = <'+ ontology_url + uri +'>)} UNION' \
-            '{ ?s ?q ?o. <'+ ontology_url + uri +'> <'+ embodied_in +'> ?manifestation. FILTER ( ?s = ?manifestation || ?o = ?manifestation )} UNION' \
-            '{ ?s ?q ?o. <'+ ontology_url + uri +'> <'+ has_related_event +'> ?event. FILTER ( ?s = ?event || ?o = ?event )} }'
+        query = 'DELETE { ?s ?q ?o } WHERE { {?s ?q ?o.' \
+                        ' <'+ uri +'> <'+ has_related_event +'> ?event. ' \
+                        'OPTIONAL { <'+ uri +'> <'+ embodied_in +'> ?manifestation. }' \
+                        'FILTER ( ?s in ( <'+ uri +'>, ?manifestation,  ?event) || ?o in ( <'+ uri +'>, ?manifestation,  ?event) )} }'
         sparql_update.method = 'POST'
         sparql_update.setQuery(query)
 
         try :
             sparql_update.query()
             if request.method == "DELETE":
-                return 'Success', 204
+                return 'Expression deleted', 200
         except Exception as e:
             abort(400, e)
 
     # TODO insertovanje posle PUT
+    rdf_qraph = parse_xml(request.data)
 
-    return 'delete'
+    return rdf_qraph
 
 
 # endpoint for Work   -->   #/akn/rs/act/zakon/2017-09-27/2017-88-3634
-@app.route('/akn/rs/act/<subtype>/<date_1>/<number>', methods=['GET', 'DELETE', 'PUT'])
+@app.route('/akn/rs/act/<subtype>/<date_1>/<number>', methods=['GET', 'DELETE', 'PUT', 'POST'])
 def fetch_work(subtype, date_1, number):
-    uri = '#/akn/rs/act/' + subtype + '/' + date_1 + '/' + number
+    uri = ontology_url  + '#/akn/rs/act/' + subtype + '/' + date_1 + '/' + number
     
     # get only returns data
     if request.method == "GET":
         try :
-            query = 'JSON {"expression": ?expression} WHERE { <'+ ontology_url + uri +'> <'+ is_realized_through +'> ?expression. ' \
+            query = 'JSON {"expression": ?expression} WHERE { <' + uri +'> <'+ is_realized_through +'> ?expression. ' \
                                                 '?expression <' + has_date + '> ?date. ' \
                                                 '?expression <'+ has_language +'> <'+ default_lng +'>. } ORDER BY DESC (?date)'
             sparql_query.setQuery(query)
@@ -159,21 +176,30 @@ def fetch_work(subtype, date_1, number):
         except Exception as e:
             abort(400, e)
 
-    # deleting of all connected stuff wit individual
+    # deleting of all connected stuff with individual
     if request.method == "PUT" or request.method == "DELETE":
 
-        # TODO brisanje za work
+        query = 'DELETE { ?s ?q ?o } WHERE { { ?s ?q ?o.' + \
+                        ' <'+ uri +'> <'+ has_related_event +'> ?work_ev. ' + \
+                        'OPTIONAL { <'+ uri +'> <'+ is_realized_through +'> ?exp. } ' + \
+                        'OPTIONAL {?exp' +' <'+ has_related_event +'> ?exp_ev. } ' + \
+                        'OPTIONAL { ?exp' +' <'+ embodied_in +'> ?man. } ' + \
+                        'FILTER (?s in ( <'+ uri +'> , ?work_ev, ?exp, ?exp_ev, ?man) || ' + \
+                                '?o in ( <'+ uri +'> , ?work_ev, ?exp, ?exp_ev, ?man) )} }'
+        sparql_update.method = 'POST'
+        sparql_update.setQuery(query)
 
         try :
-            sparql_update.query()
+            sparql_update.query().convert()
             if request.method == "DELETE":
                 return 'Success', 204
         except Exception as e:
             abort(400, e)
 
     # TODO insertovanje posle PUT
+    rdf_qraph = parse_xml(request.data)
 
-    return 'delete'
+    return rdf_qraph
 
 
 
