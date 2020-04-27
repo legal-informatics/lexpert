23981af124b644418ff30c6b5b800d1dab7203d4
Marko Vukovic <44498569+vukovic-marko@users.noreply.github.com>
2020-04-27 18:46:43 +0200

    Implementirano dodavanje i dovrsena izmena dokumenata

--- browser/flask_backend/server_1/flaskr/run_server.py
+++ browser/flask_backend/server_1/flaskr/run_server.py
@@ -6,6 +6,8 @@ import xmltodict
 import lxml.etree as ET
 import pdfkit
 
+import uuid
+
 app = Flask(__name__)
 
 sparql_query = SPARQLWrapper('http://localhost:3030/test/query')
@@ -15,15 +17,94 @@ sparql_update = SPARQLWrapper('http://localhost:3030/test/update')
 sparql_update.method = 'POST'
 
 # constants
+named_individual = 'http://www.w3.org/2002/07/owl#NamedIndividual'
+
+type = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+
 content = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#content'
-embodied_in = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#is_embodied_in'
 has_date = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#has_date'
-has_related_event = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#has_related_event'
+has_name = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#has_name'
+has_number = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#has_number'
+
 is_realized_through = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#is_realized_through'
+is_embodied_in = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#is_embodied_in'
+
+has_related_event = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#has_related_event'
+
+is_of_type = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#is_of_type'
+is_of_subtype = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#is_of_subtype'
+has_subregister = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#has_subregister'
+has_group = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#has_group'
+has_area = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#has_area'
+has_country = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#has_country'
+
 has_language = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#has_language'
 
-ontology_url = 'http://www.semanticweb.org/marko/ontologies/2020/3/untitled-ontology-36'
+is_in_format = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#is_in_format'
+
+has_responsible_role = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#has_responsible_role'
+is_of_eventtype = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#is_of_event_type'
+
+is_of_role = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#is_role_of'
+
+ontology_url = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined'
+core_url = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_core'
 default_lng = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_core#srp'
+
+def event(document):
+    """kreiranje dogadjaja za prosledjeni document (work ili expression)"""
+
+    query = ''
+
+    # Proverava da li u sparql bazi postoji trazena uloga
+    role = document['FRBRauthor']['@as'][1:]
+    ask_query = f'''
+        SELECT ?s {{
+            ?s <{is_of_role}> <{ontology_url}{document['FRBRauthor']['@href']}>.
+            ?s <{type}> <{ontology_url}#{role.capitalize()}>.
+        }}
+    '''
+
+    sparql_query.setQuery(ask_query)
+
+    try:
+        response = sparql_query.query().convert()
+
+        author_iri = ''
+
+        if len(response['results']['bindings']) == 0:
+            author_iri = f'{ontology_url}#{role}-{str(uuid.uuid1()).split("-")[0]}'
+
+            query += f'''
+                <{author_iri}> <{type}> <{ontology_url}#{role.capitalize()}>.
+                <{author_iri}> <{type}> <{named_individual}>.
+                <{author_iri}> <{is_of_role}> <{ontology_url}{document['FRBRauthor']['@href']}>.
+            '''
+        else:
+            author_iri = response['results']['bindings'][0]['s']['value']
+
+        event_iri = f'{core_url}#event-{str(uuid.uuid1()).split("-")[0]}'
+
+        event_type = ''
+        if (role == 'author'):
+            event_type = 'eventtype_001'
+        elif (role == 'editor'):
+            event_type = 'eventtype_003'
+        else:
+            abort(400, 'Event is applicable to FRBRWork and FRBRExpression')
+
+        query += f'''
+            <{event_iri}> <{type}> <{named_individual}>.
+            <{event_iri}> <{type}> <{core_url}#TLCEvent>.
+            <{event_iri}> <{has_responsible_role}> <{author_iri}>.
+            <{event_iri}> <{has_date}> "{document['FRBRdate']['@date']}"^^xsd:date.
+            <{event_iri}> <{is_of_eventtype}> <{ontology_url}#{event_type}>.
+            <{ontology_url}#{document['FRBRuri']['@value']}> <{has_related_event}> <{event_iri}>.
+            '''
+    except Exception as e:
+        abort(400, e)
+
+    return query
 
 # function to transform data to wanted format
 def transform(data, ret_format):
@@ -52,19 +133,145 @@ def transform(data, ret_format):
     return data
 
 
-def parse_xml(data):
+def parse_xml(request):
+    data = request.data
     content_dict = xmltodict.parse(data)
 
-    # TODO parsiranje xml-a i vracanje rdf grafa za ubacivanje u Fuseki
+    meta = content_dict['meta']
+    identification = meta['identification']
+    publication = meta['publication']
+    classification = meta['classification']
+    work = identification['FRBRWork']
+    expression = identification['FRBRExpression']
+    manifestation = identification['FRBRManifestation']
 
-    # INSERT DATA{ <http://example/book3> dc:title "A new book"} primer inserta
+    manifestation_uri = ontology_url + '#' + manifestation['FRBRuri']['@value']
+    expression_uri = ontology_url + '#' + expression['FRBRuri']['@value']
+    work_uri = ontology_url + '#' + work['FRBRuri']['@value']
 
-    # iz content_dict treba samo izvuci podatke i napraviti sve odgovarajuce triplete, nezaboraviti i triplete kao:
-    # <http://www.semanticweb.org/marko/ontologies/2020/3/untitled-ontology-36#/akn/rs/act/zakon/2017-09-27/2017-88-3634> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#NamedIndividual>
-    # <http://www.semanticweb.org/marko/ontologies/2020/3/untitled-ontology-36#/akn/rs/act/zakon/2017-09-27/2017-88-3634> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_core#FRBRWork>
+    cont = data.decode("utf-8")
+    cont_fix = "".join(cont.splitlines()).replace("\"", "\\\"")
 
-    return content_dict # moze da vrati i odgovor da li je insertovao ili da vrati string pa u svkom od endpointa da se radi insertovanje
+    path_uri = ontology_url + '#' + request.path
 
+    # ne dozvoljava dodavanje dokumenta ukoliko se path uri razlikuje od manifestation uri-ja u dokumentu
+    if (path_uri != manifestation_uri):
+        abort(400, 'Path uri does not match document uri!')
+
+    # ne dozvoljava dodavanje dokumenta ukoliko expression uri ne pocinje work uri-jem
+    if (not expression_uri.startswith(work_uri)):
+        abort(400, 'Expression uri does not start with work uri!')
+
+    # ne dozvoljava dodavanje dokumenta ukoliko manifestation uri ne pocinje expression uri-jem
+    if (not manifestation_uri.startswith(expression_uri)):
+        abort(400, 'Manifestation uri does not start with expression uri!')
+
+    # ne dozvoljava se dodavanje dokumenta ukoliko u bazi vec postoji dokument sa istim urijem
+    if request.method == 'POST':
+        ask_query = f'''
+            ASK {{
+                <{manifestation_uri}> <{type}> <{core_url}#FRBRManifestation>
+            }}
+        '''
+
+        sparql_query.setQuery(ask_query)
+
+        try:
+            response = sparql_query.query().convert()
+
+            if response['boolean']:
+                abort(400, 'Document with same IRI already exists!')
+        except Exception as e:
+            abort(400, e)
+
+    query = f'''
+        PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
+        INSERT DATA {{
+    '''
+
+    # proverava postoji li vec u bazi work uri iz dokumenta, kako bi se sprecilo dupliranje podataka
+    # pre svega dogadjaja ciji se nazivi dobijaju na osnovu trenutnog vremena
+    ask_query = f'''
+            SELECT ?s {{
+                <{work_uri}> <{type}> <{core_url}#FRBRWork>.
+            }}
+        '''
+
+    sparql_query.setQuery(ask_query)
+
+    try:
+        response = sparql_query.query().convert()
+
+        author_iri = ''
+
+        if len(response['results']['bindings']) == 0:
+            query += f'''
+                <{work_uri}> <{type}> <{named_individual}>.
+                <{work_uri}> <{type}> <{core_url}#FRBRWork>.
+                <{work_uri}> <{has_date}> "{work['FRBRdate']['@date']}"^^xsd:date.
+                <{work_uri}> <{has_name}> "{work['FRBRname']['@value']}"^^xsd:string.
+                <{work_uri}> <{has_number}> "{work['FRBRnumber']['@value']}"^^xsd:string.
+                <{work_uri}> <{is_of_type}> <http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#act>.
+                <{work_uri}> <{is_of_subtype}> <{core_url}#{work['FRBRsubtype']['@value']}>.
+                <{work_uri}> <{has_country}> <{core_url}#{work['FRBRcountry']['@value']}>.
+            '''
+
+            for keyword in classification['keyword']:
+                if 'subregister' in keyword['@value']:
+                    query += f'''
+                        <{work_uri}> <{has_subregister}> <{ontology_url}#{keyword['@value']}>.
+                    '''
+                elif 'group' in keyword['@value']:
+                    query += f'''
+                        <{work_uri}> <{has_group}> <{ontology_url}#{keyword['@value']}>.
+                    '''
+                if 'area' in keyword['@value']:
+                    query += f'''
+                        <{work_uri}> <{has_area}> <{ontology_url}#{keyword['@value']}>.
+                    '''
+            query += event(work)
+    except Exception as e:
+        abort(400, e)
+
+    # proverava postoji li vec u bazi expression uri iz dokumenta, kako bi se sprecilo dupliranje podataka
+    # pre svega dogadjaja ciji se nazivi dobijaju na osnovu trenutnog vremena
+    ask_query = f'''
+        SELECT ?s {{
+            <{expression_uri}> <{type}> <{core_url}#FRBRExpression>.
+        }}
+    '''
+
+    sparql_query.setQuery(ask_query)
+
+    try:
+        response = sparql_query.query().convert()
+
+        author_iri = ''
+
+        if len(response['results']['bindings']) == 0:
+            query += f'''
+                        <{expression_uri}> <{type}> <{named_individual}>.
+                        <{expression_uri}> <{type}> <{core_url}#FRBRExpression>.
+                        <{expression_uri}> <{has_date}> "{expression['FRBRdate']['@date']}"^^xsd:date.
+                        <{expression_uri}> <{has_language}> <{core_url}#{expression['FRBRlanguage']['@language']}>.
+                        <{work_uri}> <{is_realized_through}> <{expression_uri}>.
+                    '''
+            query += event(expression)
+    except Exception as e:
+        abort(400, e)
+
+    query += f'''
+        <{manifestation_uri}> <{type}> <{named_individual}>.
+        <{manifestation_uri}> <{type}> <{core_url}#FRBRManifestation>.
+        <{manifestation_uri}> <{has_date}> "{manifestation['FRBRdate']['@date']}"^^xsd:date.
+        <{manifestation_uri}> <{is_in_format}> <{core_url}#{manifestation['FRBRformat']['@value']}>.
+        <{manifestation_uri}> <{content}> "{cont_fix}"^^xsd:string.
+        <{expression_uri}> <{is_embodied_in}> <{manifestation_uri}>.
+    '''
+
+    query += f'''}}'''
+
+    return query
 
 @app.route('/simple_search')
 def simple_search():
@@ -137,13 +344,33 @@ def fetch_manifestation(subtype, date_1, number, lng, date_2, date_3, ret_format
             sparql_update.query()
             if request.method == "DELETE":
                 return 'Manifestation deleted', 200
+
+            if request.method == "PUT":
+                query = parse_xml(request)
+
+                sparql_update.setQuery(query)
+
+                try:
+                    sparql_update.query()
+                except Exception as e:
+                    abort(400, e)
+
+                return 'Succesfully edited', 200
         except Exception as e:
             abort(400, e)
 
-    # TODO insertovanje posle PUT
-    rdf_qraph = parse_xml(request.data)
+    if request.method == "POST":
 
-    return rdf_qraph
+        query = parse_xml(request)
+
+        sparql_update.setQuery(query)
+
+        try:
+            sparql_update.query()
+        except Exception as e:
+            abort(400, e)
+
+        return 'Succesfully added', 200
 
 
 # endpoint for Expressions   -->   #/akn/rs/act/zakon/2017-09-27/2017-88-3634/srp@2017-09-29
@@ -153,7 +380,7 @@ def fetch_expression(subtype, date_1, number, lng, date_2):
     
     # get only returns data
     if request.method == "GET":
-        query = 'JSON {"data": ?data} WHERE { <'+ uri +'> <'+ embodied_in +'> ?manifestation. ' \
+        query = 'JSON {"data": ?data} WHERE { <'+ uri +'> <'+ is_embodied_in +'> ?manifestation. ' \
                                             '?manifestation <' + has_date + '> ?date. ' \
                                             '?manifestation <' + content + '> ?data. } ORDER BY DESC (?date)'
         sparql_query.setQuery(query)
@@ -168,22 +395,45 @@ def fetch_expression(subtype, date_1, number, lng, date_2):
     if request.method == "PUT" or request.method == "DELETE":
         query = 'DELETE { ?s ?q ?o } WHERE { {?s ?q ?o.' \
                         ' <'+ uri +'> <'+ has_related_event +'> ?event. ' \
-                        'OPTIONAL { <'+ uri +'> <'+ embodied_in +'> ?manifestation. }' \
+                        'OPTIONAL { <'+ uri +'> <'+ is_embodied_in +'> ?manifestation. }' \
                         'FILTER ( ?s in ( <'+ uri +'>, ?manifestation,  ?event) || ?o in ( <'+ uri +'>, ?manifestation,  ?event) )} }'
+
         sparql_update.method = 'POST'
         sparql_update.setQuery(query)
 
         try :
             sparql_update.query()
+
             if request.method == "DELETE":
                 return 'Expression deleted', 200
+
+            if request.method == "PUT":
+                query = parse_xml(request)
+
+                sparql_update.setQuery(query)
+
+                try:
+                    sparql_update.query()
+                except Exception as e:
+                    abort(400, e)
+
+                return 'Succesfully edited', 200
+
         except Exception as e:
             abort(400, e)
 
-    # TODO insertovanje posle PUT
-    rdf_qraph = parse_xml(request.data)
+    if request.method == "POST":
 
-    return rdf_qraph
+        query = parse_xml(request)
+
+        sparql_update.setQuery(query)
+
+        try:
+            sparql_update.query()
+        except Exception as e:
+            abort(400, e)
+
+        return 'Succesfully added', 200
 
 
 # endpoint for Work   -->   #/akn/rs/act/zakon/2017-09-27/2017-88-3634
@@ -200,7 +450,7 @@ def fetch_work(subtype, date_1, number):
             sparql_query.setQuery(query)
             uri = sparql_query.query().convert()[0]['expression']
 
-            query = 'JSON {"data": ?data} WHERE { <'+ uri +'> <'+ embodied_in +'> ?manifestation. ' \
+            query = 'JSON {"data": ?data} WHERE { <'+ uri +'> <'+ is_embodied_in +'> ?manifestation. ' \
                                                 '?manifestation <' + has_date + '> ?date. ' \
                                                 '?manifestation <' + content + '> ?data. } ORDER BY DESC (?date)'
             sparql_query.setQuery(query)
@@ -217,9 +467,10 @@ def fetch_work(subtype, date_1, number):
                         ' <'+ uri +'> <'+ has_related_event +'> ?work_ev. ' + \
                         'OPTIONAL { <'+ uri +'> <'+ is_realized_through +'> ?exp. } ' + \
                         'OPTIONAL {?exp' +' <'+ has_related_event +'> ?exp_ev. } ' + \
-                        'OPTIONAL { ?exp' +' <'+ embodied_in +'> ?man. } ' + \
+                        'OPTIONAL { ?exp' +' <'+ is_embodied_in +'> ?man. } ' + \
                         'FILTER (?s in ( <'+ uri +'> , ?work_ev, ?exp, ?exp_ev, ?man) || ' + \
                                 '?o in ( <'+ uri +'> , ?work_ev, ?exp, ?exp_ev, ?man) )} }'
+
         sparql_update.method = 'POST'
         sparql_update.setQuery(query)
 
@@ -227,14 +478,34 @@ def fetch_work(subtype, date_1, number):
             sparql_update.query().convert()
             if request.method == "DELETE":
                 return 'Success', 204
+
+            if request.method == "PUT":
+                query = parse_xml(request)
+
+                sparql_update.setQuery(query)
+
+                try:
+                    sparql_update.query()
+                except Exception as e:
+                    abort(400, e)
+
+                return 'Succesfully edited', 200
+
         except Exception as e:
             abort(400, e)
 
-    # TODO insertovanje posle PUT
-    rdf_qraph = parse_xml(request.data)
+    if request.method == "POST":
 
-    return rdf_qraph
+        query = parse_xml(request)
 
+        sparql_update.setQuery(query)
+
+        try:
+            sparql_update.query()
+        except Exception as e:
+            abort(400, e)
+
+        return 'Succesfully added', 200
 
 if __name__ == '__main__':
     app.run(debug = True)
