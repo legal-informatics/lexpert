392cc14d89a9d7c7827de303df1c5837bf522f95
Filip Jakovljevic <fillix96@gmail.com>
2020-05-19 17:03:52 +0200

    refactoring koda

--- browser/flask_backend/server_1/flaskr/run_server.py
+++ browser/flask_backend/server_1/flaskr/run_server.py
@@ -1,6 +1,7 @@
 import os 
 from flask import Flask, url_for, request, jsonify, abort
 from SPARQLWrapper import SPARQLWrapper, XML, JSON
+from SPARQLWrapper.SPARQLExceptions import QueryBadFormed, EndPointNotFound
 
 import xmltodict
 import lxml.etree as ET
@@ -19,7 +20,7 @@ sparql_update.method = 'POST'
 # constants
 named_individual = 'http://www.w3.org/2002/07/owl#NamedIndividual'
 
-type = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+type_stavec = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' # Nije moglo samo da stoji type
 
 content = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#content'
 has_date = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#has_date'
@@ -29,7 +30,7 @@ has_number = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combin
 is_realized_through = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#is_realized_through'
 is_embodied_in = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#is_embodied_in'
 
-has_related_event = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#has_related_event'
+has_related_event = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#{ontology_url}#is_embodied_in'
 
 is_of_type = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#is_of_type'
 is_of_subtype = 'http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#is_of_subtype'
@@ -105,33 +106,6 @@ def event(document):
         abort(400, e)
 
     return query
-
-# function to transform data to wanted format
-def transform(data, ret_format):
-
-    if ret_format == 'xml':
-        return data, 200, {'content-type': 'application/xml'}
-
-
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    dom = ET.XML(data)
-    xslt = ET.parse(dir_path + '/static/akoma_to_xhtml.xsl')
-    transform = ET.XSLT(xslt)
-    newdom = transform(dom)
-    xhtml = ET.tostring(newdom, pretty_print=True, encoding='utf-8')
-
-    if ret_format == 'xhtml':
-        return xhtml, 200, {'content-type': 'application/xhtml+xml'}
-        
-
-    if ret_format == 'pdf':
-        conf = pdfkit.configuration(wkhtmltopdf='C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe')
-        options = {'encoding': "UTF-8"}
-        data = pdfkit.from_string(xhtml.decode("utf-8"), False, options = options, configuration = conf)
-        return data, 200, {'content-type': 'application/pdf'}
-
-    return data
-
 
 def parse_xml(request):
     data = request.data
@@ -273,16 +247,155 @@ def parse_xml(request):
 
     return query
 
+
+
+
+
+# function to transform data to wanted format
+def transform(data, ret_format):
+
+    if ret_format == 'xml':
+        return data, 200, {'content-type': 'application/xml'}
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    dom = ET.XML(data)
+    xslt = ET.parse(dir_path + '/static/akoma_to_xhtml.xsl')
+    transform = ET.XSLT(xslt)
+    newdom = transform(dom)
+    xhtml = ET.tostring(newdom, pretty_print=True, encoding='utf-8')
+
+    if ret_format == 'xhtml':
+        return xhtml, 200, {'content-type': 'application/xhtml+xml'}
+
+    if ret_format == 'pdf':
+        conf = pdfkit.configuration(wkhtmltopdf='C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe')
+        options = {'encoding': "UTF-8"}
+        data = pdfkit.from_string(xhtml.decode("utf-8"), False, options = options, configuration = conf)
+        return data, 200, {'content-type': 'application/pdf'}
+
+    return data
+
+# function that sends queries to Apache Jena Server
+def crud_operations(request, get_query, del_query, ret_format):
+    if request.method == "GET":
+        print('\nsending sparql to Jena: ', get_query)
+        sparql_query.setQuery(get_query)
+        
+        try :
+            response = sparql_query.query().convert()
+            return transform(response[0]['data'], ret_format)
+        except Exception as e:
+            if type(e) is QueryBadFormed:
+                abort(400, e)
+            elif len(response) == 0:
+                abort(404, 'Individual with that URI was not found')
+            else:
+                abort(500, e)
+    
+    if request.method == "DELETE":
+        print('\nsending sparql to Jena: ', del_query)
+        sparql_update.setQuery(del_query)
+        try:
+            sparql_update.query()
+            return 'Deleted', 200
+        except Exception as e:
+            abort(500, e)
+
+    if request.method == "PUT":
+        insert_query = parse_xml(request)
+        try:
+            sparql_update.setQuery(del_query)
+            sparql_update.query()
+
+            sparql_update.setQuery(insert_query)
+            sparql_update.query()
+            return 'Individual successfully updated', 200
+        except Exception as e:
+            abort(400, e)
+
+    if request.method == "POST":
+        sparql_update.setQuery(insert_query)
+        try:
+            sparql_update.query()
+            return 'Individual successfully added', 200
+        except Exception as e:
+            abort(400, e) 
+
+
+# endpoint for Manifestations   -->   #/akn/rs/act/zakon/2017-88-3634/srp@2017-09-3634/xml
+@app.route('/akn/<country>/<type_>/<subtype>/<work_id>/<lng>@<exp_id>/<ret_format>', methods=['GET', 'DELETE', 'PUT', 'POST'])
+def fetch_manifestation(country, type_, subtype, work_id, lng, exp_id, ret_format):
+    uri = f'{ontology_url}#/akn/{country}/{type_}/{subtype}/{work_id}/{lng}@{exp_id}/{ret_format}'
+    get_query = f'JSON {{ "data": ?data }} WHERE {{ <{uri}> <{ontology_url}#content> ?data.}}'
+    del_query = f'DELETE {{ ?s ?q ?o }} WHERE {{ ?s ?q ?o. FILTER ( ?s = <{uri}> || ?o = <{uri}>) }}'
+    
+    return crud_operations(request, get_query, del_query, ret_format)
+
+# endpoint for Expressions   -->   #/akn/rs/act/zakon/2017-88-3634/srp@2017-09-3634
+@app.route('/akn/<country>/<type_>/<subtype>/<work_id>/<lng>@<exp_id>', methods=['GET', 'DELETE', 'PUT', 'POST'])
+def fetch_expression(country, type_, subtype, work_id, lng, exp_id):
+    uri = f'{ontology_url}#/akn/{country}/{type_}/{subtype}/{work_id}/{lng}@{exp_id}'
+    get_query = f'''JSON {{ "data": ?data}} WHERE {{ <{uri}> <{ontology_url}#is_embodied_in> ?manifestation. 
+                                                    ?manifestation <{ontology_url}#has_date> ?date. 
+                                                    ?manifestation <{ontology_url}#content> ?data. }} ORDER BY DESC (?date)'''
+    
+    del_query = f'''DELETE {{ ?s ?q ?o }} WHERE {{ ?s ?q ?o. 
+                                                    <{uri}> <{ontology_url}#has_related_event> ?event.
+                                                    OPTIONAL {{ <{uri}> <{ontology_url}#is_embodied_in> ?manifestation. }}
+                                                    FILTER ( ?s in ( <{uri}>, ?manifestation,  ?event) || ?o in ( <{uri}>, ?manifestation,  ?event) )}}'''
+    
+    return crud_operations(request, get_query, del_query, 'xml')
+
+# endpoint for Work   -->   #/akn/rs/act/zakon/2017-88-3634
+@app.route('/akn/<country>/<type_>/<subtype>/<work_id>', methods=['GET', 'DELETE', 'PUT', 'POST'])
+def fetch_work(country, type_, subtype, work_id):
+    uri = f'{ontology_url}#/akn/{country}/{type_}/{subtype}/{work_id}'
+    get_subquery = f'''SELECT ?expression WHERE {{ <{uri}> <{ontology_url}#is_realized_through> ?expression.
+                                                    ?expression <{ontology_url}#has_date> ?date.
+                                                    ?expression <{ontology_url}#has_language> <{ontology_url}#srp>. }} ORDER BY DESC (?date) LIMIT 1'''
+    
+    get_query = f'''JSON {{ "data": ?data }} WHERE {{ {{ {get_subquery} }}
+                                                    ?expression <{ontology_url}#is_embodied_in> ?manifestation.
+                                                    ?manifestation <{ontology_url}#has_date> ?date.
+                                                    ?manifestation <{ontology_url}#content> ?data. }} ORDER BY DESC (?date)'''
+
+    del_query = f'''DELETE {{ ?s ?q ?o }} WHERE {{ ?s ?q ?o.
+                                                    <{uri}> <{ontology_url}#has_related_event> ?work_ev.
+                                                    OPTIONAL {{ <{uri}> <{ontology_url}#is_realized_through> ?exp. }}
+                                                    OPTIONAL {{ ?exp <{ontology_url}#has_related_event> ?exp_ev. }}
+                                                    OPTIONAL {{ ?exp <{ontology_url}#is_embodied_in> ?man. }}
+                                                    FILTER (?s in (<{uri}>, ?work_ev, ?exp, ?exp_ev, ?man) || ?o in (<{uri}>, ?work_ev, ?exp, ?exp_ev, ?man) ) }}'''
+
+    return crud_operations(request, get_query, del_query, 'xml')
+
+
+# exposing Apche Jena Fuseki endpoint for SPARKQL queries
+@app.route('/sparql', methods=['POST'])
+def sparql():
+    query = request.data
+    if not query:
+        abort(400, 'No sparql query has been pased')
+    
+    try :
+        sparql_query.setQuery(query)
+        response = sparql_query.query().convert()
+        return response
+    except Exception as e:
+        abort(400, e)
+
+
 @app.route('/simple_search')
 def simple_search():
+    data = request.json
 
-    # TODO search preko jednog polja, ispod je primer kako sam mislio samo imamo problem cirilice, jer 'Zakon' ne bi nasao posto mu je u imenu 'Закон' videti sta sa tim
+    query = 'JSON {"data": ?data} WHERE { ?s <http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#has_name> ?o. FILTER regex(?o, " '+ data.content +' ")}'
 
     # Select ?s where { ?s <http://www.semanticweb.org/filip/ontologies/2020/1/akn_meta_combined#has_name> ?o. FILTER regex(?o, "Закон")}
 
     # Takodje treba se dogovoriti sta se sve vraca, ocigledno uri, sto je u ovom slucaju (ili mozda samo ono posle # trebalo bi da moze), i sta jos tipa datum, vrstu, nzm ni ja
 
     return 'Najlepsa!'
+
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -299,213 +412,4 @@ def search():
     # except Exception as e:
     #     abort(400, e)
 
-    return transform(request.data, 'pdf') # trenutno mi je sluzio da testiram konverzije i sada radi sa pdf konverzijom kad mu prosledis akn xml, to je ono od cvejica, mada imaju gresaka ti akn xml-ovi
-
-
-# exposing Apche Jena Fuseki endpoint for SPARKQL queries
-@app.route('/sparql', methods=['POST'])
-def sparql():
-    query = request.data
-
-    if not query:
-        abort(400, 'No sparql query has been pased')
-    
-    sparql_query.setQuery(query)
-
-    try :
-        response = sparql_query.query().convert()
-        return response
-    except Exception as e:
-        abort(400, e)
-
-
-# endpoint for Manifestations   -->   #/akn/rs/act/zakon/2017-09-27/2017-88-3634/srp@2017-09-29/2020-03-17.xml
-@app.route('/akn/rs/act/<subtype>/<date_1>/<number>/<lng>@<date_2>/<date_3>.<ret_format>', methods=['GET', 'DELETE', 'PUT', 'POST'])
-def fetch_manifestation(subtype, date_1, number, lng, date_2, date_3, ret_format):
-    uri = ontology_url + '#/akn/rs/act/' + subtype + '/' + date_1 + '/' + number + '/' + lng + '@' + date_2 + '/' + date_3 + '.xml'
-    
-    # get only returns data
-    if request.method == "GET":
-        query = 'JSON {"data": ?data} WHERE { <'+ uri +'> <'+ content +'> ?data.}'
-        sparql_query.setQuery(query)
-
-        try :
-            response = sparql_query.query().convert()
-            return transform(response[0]['data'], ret_format) # only xml format is stored so for any other formats we need to transform them
-        except Exception as e:
-            abort(400, e)
-
-    # deleting of all connected stuff with individual
-    if request.method == "PUT" or request.method == "DELETE":
-        query = 'DELETE { ?s ?q ?o } WHERE { ?s ?q ?o. FILTER ( ?s = <'+ uri +'> || ?o = <'+ uri +'>)}'
-        sparql_update.setQuery(query)
-
-        try :
-            sparql_update.query()
-            if request.method == "DELETE":
-                return 'Manifestation deleted', 200
-
-            if request.method == "PUT":
-                query = parse_xml(request)
-
-                sparql_update.setQuery(query)
-
-                try:
-                    sparql_update.query()
-                except Exception as e:
-                    abort(400, e)
-
-                return 'Succesfully edited', 200
-        except Exception as e:
-            abort(400, e)
-
-    if request.method == "POST":
-
-        query = parse_xml(request)
-
-        sparql_update.setQuery(query)
-
-        try:
-            sparql_update.query()
-        except Exception as e:
-            abort(400, e)
-
-        return 'Succesfully added', 200
-
-
-# endpoint for Expressions   -->   #/akn/rs/act/zakon/2017-09-27/2017-88-3634/srp@2017-09-29
-@app.route('/akn/rs/act/<subtype>/<date_1>/<number>/<lng>@<date_2>', methods=['GET', 'DELETE', 'PUT', 'POST'])
-def fetch_expression(subtype, date_1, number, lng, date_2):
-    uri = ontology_url + '#/akn/rs/act/' + subtype + '/' + date_1 + '/' + number + '/' + lng + '@' + date_2
-    
-    # get only returns data
-    if request.method == "GET":
-        query = 'JSON {"data": ?data} WHERE { <'+ uri +'> <'+ is_embodied_in +'> ?manifestation. ' \
-                                            '?manifestation <' + has_date + '> ?date. ' \
-                                            '?manifestation <' + content + '> ?data. } ORDER BY DESC (?date)'
-        sparql_query.setQuery(query)
-
-        try :
-            response = sparql_query.query().convert()
-            return transform(response[0]['data'], 'xml')
-        except Exception as e:
-            abort(400, e)
-
-    # deleting of all connected stuff with individual
-    if request.method == "PUT" or request.method == "DELETE":
-        query = 'DELETE { ?s ?q ?o } WHERE { {?s ?q ?o.' \
-                        ' <'+ uri +'> <'+ has_related_event +'> ?event. ' \
-                        'OPTIONAL { <'+ uri +'> <'+ is_embodied_in +'> ?manifestation. }' \
-                        'FILTER ( ?s in ( <'+ uri +'>, ?manifestation,  ?event) || ?o in ( <'+ uri +'>, ?manifestation,  ?event) )} }'
-
-        sparql_update.method = 'POST'
-        sparql_update.setQuery(query)
-
-        try :
-            sparql_update.query()
-
-            if request.method == "DELETE":
-                return 'Expression deleted', 200
-
-            if request.method == "PUT":
-                query = parse_xml(request)
-
-                sparql_update.setQuery(query)
-
-                try:
-                    sparql_update.query()
-                except Exception as e:
-                    abort(400, e)
-
-                return 'Succesfully edited', 200
-
-        except Exception as e:
-            abort(400, e)
-
-    if request.method == "POST":
-
-        query = parse_xml(request)
-
-        sparql_update.setQuery(query)
-
-        try:
-            sparql_update.query()
-        except Exception as e:
-            abort(400, e)
-
-        return 'Succesfully added', 200
-
-
-# endpoint for Work   -->   #/akn/rs/act/zakon/2017-09-27/2017-88-3634
-@app.route('/akn/rs/act/<subtype>/<date_1>/<number>', methods=['GET', 'DELETE', 'PUT', 'POST'])
-def fetch_work(subtype, date_1, number):
-    uri = ontology_url  + '#/akn/rs/act/' + subtype + '/' + date_1 + '/' + number
-    
-    # get only returns data
-    if request.method == "GET":
-        try :
-            query = 'JSON {"expression": ?expression} WHERE { <' + uri +'> <'+ is_realized_through +'> ?expression. ' \
-                                                '?expression <' + has_date + '> ?date. ' \
-                                                '?expression <'+ has_language +'> <'+ default_lng +'>. } ORDER BY DESC (?date)'
-            sparql_query.setQuery(query)
-            uri = sparql_query.query().convert()[0]['expression']
-
-            query = 'JSON {"data": ?data} WHERE { <'+ uri +'> <'+ is_embodied_in +'> ?manifestation. ' \
-                                                '?manifestation <' + has_date + '> ?date. ' \
-                                                '?manifestation <' + content + '> ?data. } ORDER BY DESC (?date)'
-            sparql_query.setQuery(query)
-
-            response = sparql_query.query().convert()
-            return transform(response[0]['data'], 'xml')
-        except Exception as e:
-            abort(400, e)
-
-    # deleting of all connected stuff with individual
-    if request.method == "PUT" or request.method == "DELETE":
-
-        query = 'DELETE { ?s ?q ?o } WHERE { { ?s ?q ?o.' + \
-                        ' <'+ uri +'> <'+ has_related_event +'> ?work_ev. ' + \
-                        'OPTIONAL { <'+ uri +'> <'+ is_realized_through +'> ?exp. } ' + \
-                        'OPTIONAL {?exp' +' <'+ has_related_event +'> ?exp_ev. } ' + \
-                        'OPTIONAL { ?exp' +' <'+ is_embodied_in +'> ?man. } ' + \
-                        'FILTER (?s in ( <'+ uri +'> , ?work_ev, ?exp, ?exp_ev, ?man) || ' + \
-                                '?o in ( <'+ uri +'> , ?work_ev, ?exp, ?exp_ev, ?man) )} }'
-
-        sparql_update.method = 'POST'
-        sparql_update.setQuery(query)
-
-        try :
-            sparql_update.query().convert()
-            if request.method == "DELETE":
-                return 'Success', 204
-
-            if request.method == "PUT":
-                query = parse_xml(request)
-
-                sparql_update.setQuery(query)
-
-                try:
-                    sparql_update.query()
-                except Exception as e:
-                    abort(400, e)
-
-                return 'Succesfully edited', 200
-
-        except Exception as e:
-            abort(400, e)
-
-    if request.method == "POST":
-
-        query = parse_xml(request)
-
-        sparql_update.setQuery(query)
-
-        try:
-            sparql_update.query()
-        except Exception as e:
-            abort(400, e)
-
-        return 'Succesfully added', 200
-
-if __name__ == '__main__':
-    app.run(debug = True)
+    return 'testiramo' 
